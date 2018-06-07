48fd50500a4561b9135ae5df3a1c36b778c6b69a
Peter N K <pnk@avb.bg>
2018-06-07 10:09:17 +0300

    Sucessfullly performed in version 2.9.14. Ready for use. I have to finish this text when Krassi manage to fix SCR71306, because currently the acceleration key functionality it doesn't work in FFW. I this is solved, I have to inspect the Exit window through main menu in FFW

---
+++ ExitPromptText/ExitPromptText/UtilityFunctions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExitPromptText
+{
+     class PositiveResult
+    {
+        public static void PositiveText(string ExpectedText)                                      
+        {
+            Ranorex.Report.Success("Sucessfully Performed: ", ExpectedText);
+        }
+    }
+
+    class NegativeResult
+    {
+        public static void NegativeText(string ExpextedText, string Actual)
+        {
+            Ranorex.Report.Error("STOP THE VERSION: ", Actual);
+            Ranorex.Report.Error("EXPEXTED TEXT IS: ", ExpextedText);
+        }
+    }
+    class UtilityRun
+    {
+        public static void TitleText(string Actual, string Hardcoded)
+        {
+            string strActualText = Actual;
+            string strHardcodedResult = Hardcoded;
+
+            bool bExpectedText = true;
+
+            bExpectedText = strActualText.CompareTo(strHardcodedResult) == 0;
+
+            if (bExpectedText)
+            {
+                PositiveResult.PositiveText(Actual);
+            }
+            else
+            {
+                NegativeResult.NegativeText(Actual, Hardcoded);
+            }
+        }
+           
+
+    }
+}
